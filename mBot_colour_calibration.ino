e435c5a0a4d5a78b08c988211dd322da06ae7abd
adoorknob <133729599+adoorknob@users.noreply.github.com>
2023-11-02 17:34:00 +0800

    Add files via upload

--- mBot_colour_calibration.ino
+++ mBot_colour_calibration.ino
@@ -251,3 +251,38 @@ int getAvg()
 
   return total / NUM_AVG_READS;
 }
+
+char *Colour_calc(int red, int green, int blue) {
+ if (red > 230 && green > 230 && blue > 230)
+ {
+  return "white";
+ }
+
+ if (red < 50 && green < 50 && blue < 50)
+ {
+  return "black";
+ }
+ if(red > green){
+
+    if(green > blue){
+
+      if(green > 110 && blue > 110){
+
+        return "orange";
+      }
+      return "red";
+
+    }else{
+
+      return "purple";
+    }
+
+  }else if(blue > green){
+
+      return "blue";
+
+  }else{
+
+    return "green";
+  }
+}
